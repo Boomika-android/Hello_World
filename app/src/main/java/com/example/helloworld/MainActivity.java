0624a91225dfdd0027dff1085c0a5f8924e8572c
Boomika <boomika.gajendiran@rapidcare.ai>
2024-12-20 17:35:50 +0530

    delete the name

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt_view = findViewById(R.id.textView);
+
     }
 }
