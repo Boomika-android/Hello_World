6e6f5816b0e0dd737af605b6066c8d16d2fd712d
Boomika-android <boomika.gajendiran@rapidcare.ai>
2024-12-20 16:52:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.helloworld;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt_view = findViewById(R.id.textView);
+        txt_view.setText("Boomika");
     }
 }
